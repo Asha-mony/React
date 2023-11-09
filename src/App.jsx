47c50ce427e0b92e6d52e0ff1f7b5b12c3f5f6b2
Asha-mony <ynomasha5@gmail.com>
2023-11-10 00:36:59 +0600

    second

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,78 @@ import './App.css'
 
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const friends = ['asha', 'rohul', 'mendi']
 
   return (
     <div>
-      <h1>First day Intro with React</h1>
-      {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, accusantium?</p>
-      <img src="/public/vite.svg" alt="" /> */}
-      <Person></Person>
+      <h1>React Intro</h1>
+
+      <Friend
+      name={'Sajal'}
+      girlfriend={'Aliya'}>
+      </Friend>
+
+      <Friend
+      name={'Monir'}
+      girlfriend={'Tuni'}>
+      </Friend>
     </div>
+    
   )
 }
 
 export default App
 
-
-
-function Person() {
-  return (
+function Friend(props) {
+  return(
     <div>
-      <h1>Name: 'Mendi'</h1>
-      <h1>Age: 22</h1>
-      <h2>Class: 15</h2>
+      <h1>Name:{props.name}</h1>
+      <h1>Girlfriend:{props.girlfriend}</h1>
     </div>
   )
 }
+
+
+
+
+
+
+
+
+
+
+//   return (
+//     <div>
+//       <h1>First day Intro with React</h1>
+//     {
+//       friends.map ((friend, index) => <Person key={index} data= {friend}></Person>)
+//     }
+
+//      {/* <Person data='Mendi'></Person>
+//      <Person data='Rohul'></Person>
+//      <Person data='Asha'></Person> */}
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+// function Person(props) {
+//   console.log(props);
+//   return (
+//     <div>
+//       <h1>Name:{props.data}</h1>
+      
+//     </div>
+//   )
+// }
+
+
+
+// function add(a,b) {
+//   return a + b
+// }
+// add(3, 2)
